4d0e60a69a62762255fd727dcc981e97a08f2a1d
Yu-Chuan, Ciou <z111048@gmail.com>
2023-11-25 08:10:33 +0800

    3th commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const axios = require("axios");
 
 const app = express();
-const PORT = 3000; // 可以更改為您希望的端口
+const PORT = 3001; // 可以更改為您希望的端口
 
 const cors = require("cors");
 // 啟用所有 CORS 請求
@@ -13,14 +13,17 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 const LINE_NOTIFY_TOKEN = process.env.LINE_NOTIFY_TOKEN;
-console.log(LINE_NOTIFY_TOKEN);
+// console.log(LINE_NOTIFY_TOKEN);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("submit-feedback", (req, res) => {
-  const feedback = req.body.feedback;
-
-  lineNotify(feedback)
+// http://localhost:3000/submit-feedback
+app.post("/submit-feedback", (req, res) => {
+  console.log("Received body:", req.body); // 調試輸出
+  const category = req.body.category;
+  const content = req.body.content;
+  const lineNotify_text = category + "\n" + content;
+  lineNotify(lineNotify_text)
     .then(() => {
       res.send("感謝您的回饋！");
     })
@@ -35,7 +38,6 @@ function lineNotify(message) {
     "Content-Type": "application/x-www-form-urlencoded",
     Authorization: `Bearer ${LINE_NOTIFY_TOKEN}`,
   };
-  console.log(console);
   const data = new URLSearchParams();
   data.append("message", message);
 
